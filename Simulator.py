63d4ff270892f7c132495f142be293dec30f4559
Ali Zeynali <alizeynalipython@gmail.com>
2019-06-09 20:09:19 +0430

    version 2.0.1

--- Simulator.py
+++ Simulator.py
@@ -67,9 +67,9 @@ def get_read_in_range(all_reads, window_size, list_of_n, p_tumor, list_of_positi
             end_pos = begin_pos + window_size
             window_read_tumor = []
             window_read_normal = []
-            if len(list_of_positions_errors) > 0 and begin_pos <= list_of_positions_errors[0] <= end_pos:
+            if len(list_of_positions_errors) > 0 and begin_pos <= list_of_positions_errors[0][0] <= end_pos:
                 list_val = list_of_positions_errors.pop(0)
-                new_position_error.append([list_val[0], p_normal * list_val[1] + p_tumor * list_val[2]])
+                new_position_error.append([list_val[0], p_normal * list_val[1] + p_tumor_val * list_val[2]])
             while len(all_reads) > 0 and all_reads[0][3] < begin_pos:
                 all_reads.pop(0)
             while len(all_reads) > 0 and all_reads[0][2] <= end_pos:
@@ -230,21 +230,34 @@ def read_percentage_list(path):
     return list_of_p
 
 
+def read_error_rate_list(path):
+    list_of_p = []
+    with open(path) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+                continue
+            list_of_p.append(float(row[0]))
+            line_count += 1
+    return list_of_p
+
+
 if __name__ == "__main__":
     initial_time = time.time()
     # normal_path = sys.argv[1]
     # tumor_path = sys.argv[2]
     # output_name = sys.argv[3]
-    output_name = 'Simulator_v2'
+    output_name = 'Simulator_v2.0.1'
     tumor_path = "test_sam.sam"
     normal_path = "p1_blood.sam"
     tumor_sam = pysam.AlignmentFile(tumor_path, "r")
     normal_sam = pysam.AlignmentFile(normal_path, "r")
     all_reads = {}
-    # TODO p_tumor , p_nomral, list_of_n should be obtained by the code
     list_of_p_tumor = read_percentage_list('ctPercentage_sample.csv')
-    list_of_normal_error_rates = read_percentage_list('bloodSmallPeak.csv')
-    list_of_tissue_error_rates = read_percentage_list('tissueSmallPeak.csv')
+    list_of_normal_error_rates = read_error_rate_list('bloodSmallPeak.csv')
+    list_of_tissue_error_rates = read_error_rate_list('tissueSmallPeak.csv')
     window_size = 300
     p_tumor = 0.2
     number_of_input_normal_reads = 0
@@ -252,6 +265,8 @@ if __name__ == "__main__":
     number_of_simulated_normal_reads = 0
     number_of_simulated_tissue_reads = 0
     total_positions = 1250103
+    if len(list_of_normal_error_rates) < total_positions:
+        total_positions = len(list_of_normal_error_rates) - 1
     header = normal_sam.text
     print("Reading normal file...")
     for normal_read in normal_sam.fetch():
@@ -302,14 +317,17 @@ if __name__ == "__main__":
         print("\t\tSimulating chromosome:\t", chr)
         begin_pos = chr_reads[0][2]
         end_pos = chr_reads[-1][3]
-        positions = numpy.random.choice(range(begin_pos, end_pos), int(total_positions / 23), replace=False)
+        positions = sorted(random.sample(range(begin_pos, end_pos), int(total_positions / 24)))
         list_of_n = read_coverage_list('chr_cov/chr_cov/{0}_coverage.csv'.format(chr))
         # error_positions = [[72550, 0.5, 0.5], [110400, 0.5, 0.5], [142700, 0.5, 0.5], [142800, 0.5, 0.5],
         #                    [142900, 0.5, 0.5], [143100, 0.5, 0.5], [143300, 0.5, 0.5], [144100, 0.5, 0.5],
         #                    [144300, 0.5, 0.5]]
         error_positions = []
         for i in range(len(positions)):
-            error_positions.append([positions[i], list_of_normal_error_rates.pop(0), list_of_tissue_error_rates.pop(0)])
+            error_positions.append([positions[i], list_of_normal_error_rates[i], list_of_tissue_error_rates[i]])
+        l = len(error_positions)
+        list_of_normal_error_rates = list_of_normal_error_rates[l:]
+        list_of_tissue_error_rates = list_of_tissue_error_rates[l:]
         read_in_range, normal_sim, tissue_sim = get_read_in_range(chr_reads, window_size, list_of_n, list_of_p_tumor,
                                                                   error_positions)
         number_of_simulated_normal_reads += normal_sim
