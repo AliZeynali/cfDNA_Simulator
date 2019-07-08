f4ca3216570e746a8606e4923d6e18158f8ee214
Ali Zeynali <alizeynalipython@gmail.com>
2019-07-08 14:11:23 +0430

    version 2.2.1 released

--- Simulator.py
+++ Simulator.py
@@ -40,106 +40,78 @@ def has_intersection(begin1, end1, begin2, end2):
         exit(1)
 
 
-def get_read_in_range(all_reads, window_size, list_of_n, p_tumor, list_of_positions_errors):
+def get_read_in_range(all_reads, window_size, list_of_error_positions):
     '''
 
     :param all_reads: list of [ type (tumor or normal ), chromosome, begin_pos, end_pos, raw_read_string]
     :param begin: first position in this chromosome
     :param window_size:
-    :param list_of_n:
-    :param p_tumor
-    :param p_normal
-    :param list_of_positions_errors: list of [pos, normal error rate, tissue error rate]
+    :param list_of_error_positions: list of pos of errors
     :return:
     '''
-
+    p_shape, p_scale = 0.7532623, 3.4025548  # For the p_tumor distribution
+    n_size, n_prob = 138.7844083, 0.2221451
+    cfDNA_mu, cfDNA_sigma = -1.650449, 0.3348813
+    tissue_mu, tissue_sigma = -2.161420, 0.4807095
+    blood_mu, blood_sigma = -1.903177, 0.3766689
     reads = []
     normal_simulated = 0
     tissue_simulated = 0
     new_position_error = []
-    if list_of_n != None:
-        while len(all_reads) > 0:
-            if len(p_tumor) > 0:
-                p_tumor_val = p_tumor.pop(0)
-            else:
-                p_tumor_val = 0.2 #default p_tumor
-            p_normal = 1 - p_tumor_val
-            i = int(all_reads[0][2] / window_size)
-            if len(list_of_n) > 0:
-                n = list_of_n.pop(0)
-            else:
-                n = 100 #default n
-            begin_pos = i * window_size
-            end_pos = begin_pos + window_size
-            window_read_tumor = []
-            window_read_normal = []
-            if len(list_of_positions_errors) > 0 and begin_pos <= list_of_positions_errors[0][0] <= end_pos:
-                list_val = list_of_positions_errors.pop(0)
-                new_position_error.append([list_val[0], p_normal * list_val[1] + p_tumor_val * list_val[2]])
-            while len(all_reads) > 0 and all_reads[0][3] < begin_pos:
-                all_reads.pop(0)
-            while len(all_reads) > 0 and all_reads[0][2] <= end_pos:
-                read = all_reads.pop(0)
-                if read[0] == 'tumor':
-                    window_read_tumor.append(read[4])
-                elif read[0] == 'normal':
-                    window_read_normal.append(read[4])
-            # n = len(window_read_tumor) + len(window_read_normal)
-            if len(window_read_normal) == 0:
-                selected_normal_reads = []
-            else:
-                selected_normal_reads = numpy.random.choice(window_read_normal,
-                                                            min(int(n * p_normal), len(window_read_normal)),
-                                                            replace=False)
-            if len(window_read_tumor) == 0:
-                selected_tumor_reads = []
-            else:
-                selected_tumor_reads = numpy.random.choice(window_read_tumor,
-                                                           min(int(n * p_tumor_val), len(window_read_tumor)),
-                                                           replace=False)
-            for r in selected_normal_reads:
-                reads.append(r)
-            for r in selected_tumor_reads:
-                reads.append(r)
-            normal_simulated += len(selected_normal_reads)
-            tissue_simulated += len(selected_tumor_reads)
-        return add_PCR_error(reads, new_position_error), normal_simulated, tissue_simulated
-
-    # else:
-    # p_normal = 1 - p_tumor
-    # n = 300
-    # while len(all_reads) > 0:
-    #     i = int(all_reads[0][2] / window_size)
-    #     begin_pos =  i * window_size
-    #     end_pos = begin_pos + window_size
-    #     window_read_tumor = []
-    #     window_read_normal = []
-    #     while len(all_reads) > 0 and all_reads[0][3] < begin_pos:
-    #         all_reads.pop(0)
-    #     while len(all_reads) > 0 and all_reads[0][2] <= end_pos:
-    #         read = all_reads.pop(0)
-    #         if read[0] == 'tumor':
-    #             window_read_tumor.append(read[4])
-    #         elif read[0] == 'normal' and random.random() < p_normal:
-    #             window_read_normal.append(read[4])
-    #     if len(window_read_normal) == 0:
-    #         selected_normal_reads = []
-    #     else:
-    #         selected_normal_reads = numpy.random.choice(window_read_normal,
-    #                                                     min(int(n * p_normal), len(window_read_normal)),
-    #                                                     replace=False)
-    #     if len(window_read_tumor) == 0:
-    #         selected_tumor_reads = []
-    #     else:
-    #         selected_tumor_reads = numpy.random.choice(window_read_tumor,
-    #                                                    min(int(n * p_tumor), len(window_read_tumor)),
-    #                                                    replace=False)
-    #     for r in selected_normal_reads:
-    #         reads.append(r)
-    #     for r in selected_tumor_reads:
-    #         reads.append(r)
-
-    return reads
+    while len(all_reads) > 0:
+        p_tumor_val = -1
+        while (p_tumor_val <= 0 or p_tumor_val >= 1):
+            p_tumor_val = numpy.random.gamma(p_shape, p_scale, 1)[0]
+        p_normal = 1 - p_tumor_val
+        i = int(all_reads[0][2] / window_size)
+        n = 0
+        while n < 6:
+            n = numpy.random.negative_binomial(n_size, n_prob, 1)[0]
+        begin_pos = i * window_size
+        end_pos = begin_pos + window_size
+        window_read_tumor = []
+        window_read_normal = []
+        if len(list_of_error_positions) > 0 and begin_pos <= list_of_error_positions[0] <= end_pos:
+            list_val = list_of_error_positions.pop(0)
+            p_blood_error = -1
+            p_tissue_error = -1
+            while (p_blood_error <= 0 or p_blood_error >= 1):
+                cfdna_error = 10 ** (numpy.random.normal(cfDNA_mu, cfDNA_sigma, 1)[0])
+                blood_error = 10 ** (numpy.random.normal(blood_mu, blood_sigma, 1)[0])
+                p_blood_error = cfdna_error - blood_error
+            while (p_tissue_error <= 0 or p_tissue_error >= 1):
+                cfdna_error = 10 ** (numpy.random.normal(cfDNA_mu, cfDNA_sigma, 1)[0])
+                tissue_error = 10 ** (numpy.random.normal(tissue_mu, tissue_sigma, 1)[0])
+                p_tissue_error = cfdna_error - tissue_error
+            new_position_error.append([list_val, p_normal * p_blood_error + p_tumor_val * p_tissue_error])
+        while len(all_reads) > 0 and all_reads[0][3] < begin_pos:
+            all_reads.pop(0)
+        while len(all_reads) > 0 and all_reads[0][2] <= end_pos:
+            read = all_reads.pop(0)
+            if read[0] == 'tumor':
+                window_read_tumor.append(read[4])
+            elif read[0] == 'normal':
+                window_read_normal.append(read[4])
+        # n = len(window_read_tumor) + len(window_read_normal)
+        if len(window_read_normal) == 0:
+            selected_normal_reads = []
+        else:
+            selected_normal_reads = numpy.random.choice(window_read_normal,
+                                                        min(int(n * p_normal), len(window_read_normal)),
+                                                        replace=False)
+        if len(window_read_tumor) == 0:
+            selected_tumor_reads = []
+        else:
+            selected_tumor_reads = numpy.random.choice(window_read_tumor,
+                                                       min(int(n * p_tumor_val), len(window_read_tumor)),
+                                                       replace=False)
+        for r in selected_normal_reads:
+            reads.append(r)
+        for r in selected_tumor_reads:
+            reads.append(r)
+        normal_simulated += len(selected_normal_reads)
+        tissue_simulated += len(selected_tumor_reads)
+    return add_PCR_error(reads, new_position_error), normal_simulated, tissue_simulated
 
 
 def is_in_matched_region(position, cigartuples):
@@ -199,41 +171,23 @@ def read_to_string(read, rNext, pNext):
     if read[1] == True:
         for tup in read[2]:
             query = query[0:tup[0]] + tup[1] + query[tup[0] + 1:]
+    qual = str(read[0].qual)
+    if len(qual) == len(query) + 3:
+        qual = qual[2:-1]
+    elif len(qual) != len(query):
+        qual = ""
+        for _ in range(len(query)):
+            qual += "J"
+
     string = ""
     string += str(read[0].query_name) + "\t" + str(read[0].flag) + '\t' + str(read[0].reference_name) + '\t'
     string += str(read[0].get_reference_positions()[0]) + "\t" + str(read[0].mapping_quality) + '\t' + str(
         read[0].cigarstring) + '\t' + str(rNext) + '\t' + str(pNext) + '\t' + str(read[0].template_length) + '\t' + str(
-        query) + '\t' + str(read[0].qual)[2:-1]
+        query) + '\t' + qual
+    # if len(query) != len(str(read[0].qual)[2:-1]):
+    #     print("Hi")
 
     return string
-
-
-def read_coverage_list(path):
-    list_of_n = []
-    with open(path) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count == 0:
-                line_count += 1
-                continue
-            list_of_n.append(int(row[1]))
-            line_count += 1
-    return list_of_n
-
-
-def read_percentage_list(path):
-    list_of_p = []
-    with open(path) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count == 0:
-                line_count += 1
-                continue
-            list_of_p.append(float(row[1]))
-            line_count += 1
-    return list_of_p
 
 
 def read_error_rate_list(path):
@@ -251,19 +205,18 @@ def read_error_rate_list(path):
 
 
 if __name__ == "__main__":
+    version = "v2.2.1"
     initial_time = time.time()
     normal_path = sys.argv[1]
     tumor_path = sys.argv[2]
     # output_name = sys.argv[3]
-    output_name = 'Simulator_v2.1.4'
+    output_name = 'Simulator' + "_" + version
     # tumor_path = "test_sam.sam"
     # normal_path = "p1_blood.sam"
+    print("Running Simulator version:\t" + version)
     tumor_sam = pysam.AlignmentFile(tumor_path, "r")
     normal_sam = pysam.AlignmentFile(normal_path, "r")
     all_reads = {}
-    list_of_p_tumor = read_percentage_list('ctPercentage_sample.csv')
-    list_of_normal_error_rates = read_error_rate_list('bloodSmallPeak.csv')
-    list_of_tissue_error_rates = read_error_rate_list('tissueSmallPeak.csv')
     window_size = 300
     p_tumor = 0.2
     number_of_input_normal_reads = 0
@@ -271,9 +224,8 @@ if __name__ == "__main__":
     number_of_simulated_normal_reads = 0
     number_of_simulated_tissue_reads = 0
     total_positions = 1250103
-    if len(list_of_normal_error_rates) < total_positions:
-        total_positions = len(list_of_normal_error_rates) - 1
     header = normal_sam.text
+    error_poses = []
     print("Reading normal file...")
     for normal_read in normal_sam.fetch():
         try:
@@ -283,8 +235,6 @@ if __name__ == "__main__":
                 continue
             read = ['normal', normal_read.reference_name, normal_read.get_reference_positions()[0],
                     normal_read.get_reference_positions()[-1], normal_read]
-            # if normal_read.cigarstring != '150M':
-            #     print("XX")
             if normal_read.reference_name in all_reads:
                 all_reads[normal_read.reference_name].append(read)
                 number_of_input_normal_reads += 1
@@ -304,17 +254,12 @@ if __name__ == "__main__":
         begin_pos = chr_reads[0][2]
         end_pos = chr_reads[-1][3]
         positions = sorted(random.sample(range(begin_pos, end_pos), int(total_positions / 48)))
-        list_of_n = read_coverage_list('chr_cov/chr_cov/{0}_coverage.csv'.format(chr))
-        # error_positions = [[72550, 0.5, 0.5], [110400, 0.5, 0.5], [142700, 0.5, 0.5], [142800, 0.5, 0.5],
-        #                    [142900, 0.5, 0.5], [143100, 0.5, 0.5], [143300, 0.5, 0.5], [144100, 0.5, 0.5],
-        #                    [144300, 0.5, 0.5]]
+        for p in positions:
+            error_poses.append([chr,p])
         error_positions = []
         for i in range(len(positions)):
-            error_positions.append([positions[i], list_of_normal_error_rates[i], list_of_tissue_error_rates[i]])
-        l = len(error_positions)
-        list_of_normal_error_rates = list_of_normal_error_rates[l:]
-        list_of_tissue_error_rates = list_of_tissue_error_rates[l:]
-        read_in_range, normal_sim, tissue_sim = get_read_in_range(chr_reads, window_size, list_of_n, list_of_p_tumor,
+            error_positions.append(positions[i])
+        read_in_range, normal_sim, tissue_sim = get_read_in_range(chr_reads, window_size,
                                                                   error_positions)
         number_of_simulated_normal_reads += normal_sim
         number_of_simulated_tissue_reads += tissue_sim
@@ -345,8 +290,6 @@ if __name__ == "__main__":
             # print(e)
             continue
     del (tumor_sam)
-    list_of_normal_error_rates = read_error_rate_list('bloodSmallPeak.csv')
-    list_of_tissue_error_rates = read_error_rate_list('tissueSmallPeak.csv')
     print("Simulating by tissue reads...")
     for chr in all_reads:
         chr_reads = sorted(all_reads[chr], key=lambda x: x[2])
@@ -354,31 +297,26 @@ if __name__ == "__main__":
         begin_pos = chr_reads[0][2]
         end_pos = chr_reads[-1][3]
         positions = sorted(random.sample(range(begin_pos, end_pos), int(total_positions / 48)))
-        list_of_n = read_coverage_list('chr_cov/chr_cov/{0}_coverage.csv'.format(chr))
-        # error_positions = [[72550, 0.5, 0.5], [110400, 0.5, 0.5], [142700, 0.5, 0.5], [142800, 0.5, 0.5],
-        #                    [142900, 0.5, 0.5], [143100, 0.5, 0.5], [143300, 0.5, 0.5], [144100, 0.5, 0.5],
-        #                    [144300, 0.5, 0.5]]
+        for p in positions:
+            error_poses.append([chr,p])
         error_positions = []
         for i in range(len(positions)):
-            error_positions.append([positions[i], list_of_normal_error_rates[i], list_of_tissue_error_rates[i]])
-        l = len(error_positions)
-        list_of_normal_error_rates = list_of_normal_error_rates[l:]
-        list_of_tissue_error_rates = list_of_tissue_error_rates[l:]
-        read_in_range, normal_sim, tissue_sim = get_read_in_range(chr_reads, window_size, list_of_n, list_of_p_tumor,
+            error_positions.append(positions[i])
+        read_in_range, normal_sim, tissue_sim = get_read_in_range(chr_reads, window_size,
                                                                   error_positions)
         number_of_simulated_normal_reads += normal_sim
         number_of_simulated_tissue_reads += tissue_sim
         for r in read_in_range:
             sam_file_reads.append(r)
 
-    print("number of total input reads:\t", number_of_input_tissue_reads + number_of_input_normal_reads)
+    print("\nnumber of total input reads:\t", number_of_input_tissue_reads + number_of_input_normal_reads)
     print("number of normal input reads:\t", number_of_input_normal_reads)
     print("number of tissue input reads:\t", number_of_input_tissue_reads)
 
     print("number of total simulated reads:\t", len(sam_file_reads))
     print("number of normal simulated reads:\t", number_of_simulated_normal_reads)
     print("number of tissue simulated reads:\t", number_of_simulated_tissue_reads)
-    print("Writing...")
+    print("Writing to file...")
     with open('{0}_simulated.sam'.format(output_name), 'w') as writer:
         writer.write(header)
         for i in range(len(sam_file_reads)):
@@ -389,12 +327,8 @@ if __name__ == "__main__":
                 pNext = '0'
             writer.write(read_to_string(read, '=', pNext) + '\n')
     writer.close()
-    # with open('{0}_simulated.fq'.format(output_name), 'w') as writer2:
-    #     for read in sam_file_reads:
-    #         writer2.write("@{0}\n".format(str(read[0].query_name)))
-    #         writer2.write("{0}\n".format(str(read[0].query)))
-    #         writer2.write("+\n")
-    #         writer2.write("{0}\n".format(str(read[0].qual)))
-    # writer2.close()
+    with open('Error_positions.txt', 'w') as writer2:
+        for val in error_poses:
+            writer2.write(str(val[0]) + "\t" + str(val[1]) + "\n")
 
     print("Run time duration:\t{0}".format(time.time() - initial_time))
